3da7f29abcc68714ae27df96bd62e1a3aa8e89d5
Tu Tran <tu.tran@temple.edu>
2016-08-22 01:06:03 +0700

    new branch; black background; bio toggle on/off

--- static/js/app.js
+++ static/js/app.js
@@ -8,4 +8,24 @@ $(document).ready(function() {
   $(".icons").click(function() {
     $(this).addClass("animated flip");
   });
+
+  $("#info").click(function() {
+    $("#bio").css("display", "block");
+    $("#main-header").animate({
+      "margin-top": "60px"
+    });
+  });
+
+  $("#close-bio").hover(function() {
+    $(this).addClass("animated flip");
+  }, function() {
+    $(this).removeClass("animated flip");
+  });
+
+  $("#close-bio").click(function() {
+    $("#bio").css("display", "none");
+    $("#main-header").animate({
+      "margin-top": "15%"
+    });
+  });
 });
